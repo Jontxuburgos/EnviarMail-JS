13f5db7867dc9035194f0a17ddd7ca60b897cc8b
Jontxu <jonburgos4@gmail.com>
2023-11-16 11:33:00 +0100

    crear alertas y mostrarlas en HTML

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //Asignar Eventos 
     inputEmail.addEventListener('blur', validar);
@@ -10,14 +11,31 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e) {
-        if (e.target.value.trim === '') {//.trim elimina los espacios en blanco innecesarios
-            
-            
-            
-            console.log('vacio');
+        if (e.target.value.trim() === '') {//.trim elimina los espacios en blanco innecesarios
+            mostrarAlerta();
         } else {
             console.log('lleno');
         }
     };
+
+    function mostrarAlerta() {
+        //Generar Alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = 'Hubo un error';
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
+
+
+
+        //Inyectar el error al formulario
+        formulario.appendChild(error);
+        
+        ;
+    }
+
+
+
+
+
+
 });
 

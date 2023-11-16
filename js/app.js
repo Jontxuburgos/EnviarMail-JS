d5b7014329a26d184ddeb0cc2d5e96388bcc87fb
Jontxu <jonburgos4@gmail.com>
2023-11-16 13:43:11 +0100

    Implementación spinner

--- js/app.js
+++ js/app.js
@@ -13,11 +13,14 @@ document.addEventListener('DOMContentLoaded', function(){
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     //Asignar Eventos 
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+
+    formulario.addEventListener('submit', enviarEmail);
 
     btnReset.addEventListener('click', function(e) {
         e.preventDefault();
@@ -30,6 +33,14 @@ document.addEventListener('DOMContentLoaded', function(){
         formulario.reset();
         comprobarEmail();
     })
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+    };
+
 
     function validar(e) {
 
@@ -86,11 +97,11 @@ document.addEventListener('DOMContentLoaded', function(){
     function comprobarEmail() {
         if((Object.values(email).includes(''))) {
             btnSubmit.classList.add('opacity-50');
-            btnSubmit.disble = true;
+            btnSubmit.disabled = true;
             return;
         } 
         btnSubmit.classList.remove('opacity-50');
-        btnSubmit.disble = false;
+        btnSubmit.disabled = false;
 
         
     };
